f94b09a366e98945b5dc46c63c7b395d7d1c3c5a
Hunganh3012 <nguyenhunganh072017@gamil.com>
2022-08-12 09:34:48 +0700

    run git hub page

--- src/app/admin/admin.service.ts
+++ src/app/admin/admin.service.ts
@@ -1,19 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+
 @Injectable({
   providedIn: 'root'
 })
 export class AdminService {
-
+  items = [];
   constructor(private http:HttpClient) { }
   API_Product="http://localhost:3000/products";
-API_Cart="http://localhost:3000/cart"
+// API_Cart ="http://localhost:3000/cart"
   getProduct(){
     return this.http.get(`${this.API_Product}`)
   }
   addProduct(data:any){
     return this.http.post(this.API_Product,data)
-
   }
   deleteProduct(id:number){
     return this.http.delete(`${this.API_Product}/${id}`)
@@ -27,10 +27,24 @@ API_Cart="http://localhost:3000/cart"
   porductdetail(id:number){
     return this.http.get(`${this.API_Product}/${id}`)
   }
+  getCart(){
+    this.API_Product;
+  }
   getProductCart(id:number){
     return this.http.get(`${this.API_Product}/${id}`)
   }
-  // addtocart(data:any){
-  //   return this.http.post(`${this.API_Cart,data);
+  
+  // addToCart(addedItem:any) {
+  //   this.items.push(addedItem)
+  //   this.saveCart();
+  // }
+  // getItems() {
+  //   return this.items;
+  // }
+  // loadCart(): void {
+  //   this.items = JSON.parse(localStorage.getItem('cart-item')) ?? [];
+  // }
+  // saveCart(): void {
+  //   localStorage.setItem('cart_items', JSON.stringify(this.items));
   // }
 }
