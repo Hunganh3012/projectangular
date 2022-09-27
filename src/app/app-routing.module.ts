e6258e923b6bf800f9e6506b29ff4bd62d20eade
Hunganh3012 <nguyenhunganh072017@gamil.com>
2022-09-27 14:25:16 +0700

    fix # #

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -60,6 +60,6 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes, {
     preloadingStrategy: PreloadAllModules,
   }),],
-  exports: [RouterModule],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
