ea9ca796bb4e322ca9cecaa4abab2f61c8f35fcc
Hunganh3012 <nguyenhunganh072017@gamil.com>
2022-10-11 11:07:05 +0700

    popup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { placeholder } from '@cloudinary/ng';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Bán nước giải khát';
+  ngOnInit(){
+    Swal.fire({
+      title: '<p style="font-size:20px; padding-top:20px">Vui lòng nhập họ tên và xác nhận bạn đã đủ 18 tuổi để tiếp tục (theo quy định khi mua rượu bia)</p>',
+      input:'text',
+      imageUrl: 'https://images.unsplash.com/photo-1441985969846-3e7c90531139?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGJlZXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+  imageWidth: 500,
+  imageHeight: 300,
+      // html:'<input type="text" placeholder="Họ và tên *" style="outline:none; padding: 0 5px;width:100%; height:50px ;border: 1px solid #e9eaeb">',
+      showCancelButton: false,
+      confirmButtonText: 'Tôi trên 18 tuổi',
+      cancelButtonText:'Tôi ',
+      showLoaderOnConfirm: true,
+      preConfirm: (login) => {
+        return fetch(`//api.github.com/users/${login}`)
+          .then(response => {
+            if (!response.ok) {
+              throw new Error(response.statusText)
+            }
+            return response.json()
+          })
+          .catch(error => {
+            Swal.showValidationMessage(
+              `Vui lòng nhập Họ và Tên`
+            )
+          })
+      }
+      
+    })
+  }
 }
--- src/app/dist/app.component.js
+++ src/app/dist/app.component.js
@@ -8,10 +8,36 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 exports.__esModule = true;
 exports.AppComponent = void 0;
 var core_1 = require("@angular/core");
+var sweetalert2_1 = require("sweetalert2");
 var AppComponent = /** @class */ (function () {
     function AppComponent() {
         this.title = 'Bán nước giải khát';
     }
+    AppComponent.prototype.ngOnInit = function () {
+        sweetalert2_1["default"].fire({
+            title: '<p style="font-size:20px; padding-top:20px">Vui lòng nhập họ tên và xác nhận bạn đã đủ 18 tuổi để tiếp tục (theo quy định khi mua rượu bia)</p>',
+            input: 'text',
+            imageUrl: 'https://images.unsplash.com/photo-1441985969846-3e7c90531139?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGJlZXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+            imageWidth: 500,
+            imageHeight: 300,
+            // html:'<input type="text" placeholder="Họ và tên *" style="outline:none; padding: 0 5px;width:100%; height:50px ;border: 1px solid #e9eaeb">',
+            showCancelButton: false,
+            confirmButtonText: 'Tôi trên 18 tuổi',
+            cancelButtonText: 'Tôi ',
+            showLoaderOnConfirm: true,
+            preConfirm: function (login) {
+                return fetch("//api.github.com/users/" + login)
+                    .then(function (response) {
+                    if (!response.ok) {
+                        throw new Error(response.statusText);
+                    }
+                    return response.json();
+                })["catch"](function (error) {
+                    sweetalert2_1["default"].showValidationMessage("Vui l\u00F2ng nh\u1EADp H\u1ECD v\u00E0 T\u00EAn");
+                });
+            }
+        });
+    };
     AppComponent = __decorate([
         core_1.Component({
             selector: 'app-root',
